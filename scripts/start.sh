970323fb118605128f6dc72040830d9aef0b1abe
John Crossman <johncrossman@berkeley.edu>
2016-12-06 15:22:17 -0800

    COL-309, deploy script will rename forever logs using timestamp

--- scripts/start.sh
+++ scripts/start.sh
@@ -18,6 +18,6 @@ cd "${SUITEC_BASE_DIR}"
 LOG_DIR=~/log
 
 # Start the app server
-./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 start app.js
+./node_modules/.bin/forever -a -l "${LOG_DIR}/forever.log" -i "${LOG_DIR}/forever_app.log" -e "${LOG_DIR}/forever_app.log" -m 10 start app.js
 
 exit 0
